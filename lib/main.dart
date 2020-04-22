9b27abb662c698dd24e1ad63fcb96f1eed313afd
Kasun Rukmaldeniya <rukaatricks.com@gmail.com>
2020-04-23 02:09:55 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
-  return runApp(
+  runApp(
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.deepPurpleAccent,
@@ -9,10 +10,46 @@ void main() {
           title: Text('Parking Management System'),
           backgroundColor: Colors.deepPurple,
         ),
-        body: Parking(),
+        body: View(),
       ),
     ),
   );
+}
+
+class View extends StatelessWidget {
+  List<Widget> makeListWidget(AsyncSnapshot snapshot) {
+    return snapshot.data.documents.map<Widget>((document) {
+      return ListTile(
+        title: Text(document["slot"]),
+        subtitle: Text(document["availability"]),
+      );
+    }).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: StreamBuilder<QuerySnapshot>(
+          stream: Firestore.instance.collection('parking').snapshots(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.waiting:
+                return Center(child: CircularProgressIndicator());
+              default:
+                return ListView(
+                  children: makeListWidget(snapshot),
+                );
+            }
+          }),
+    );
+  }
+}
+
+class Slots extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('d');
+  }
 }
 
 class Parking extends StatefulWidget {
@@ -76,16 +113,16 @@ class _ParkingState extends State<Parking> {
           Row(
             children: <Widget>[
               slots(0),
-              slots(0),
               slots(1),
+              slots(0),
               slots(0),
             ],
           ),
           Row(
             children: [
+              slots(0),
               slots(1),
-              slots(1),
-              slots(1),
+              slots(0),
               slots(1),
             ],
           ),
